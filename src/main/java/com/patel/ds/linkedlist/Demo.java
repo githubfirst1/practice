7aa5d4f6d32952b89797d2414f16632be8dfaab0
bhupatibit <bhupatipatel009@gmail.com>
2018-01-15 09:59:11 +0530

    Added more methods

--- src/main/java/com/patel/ds/linkedlist/Demo.java
+++ src/main/java/com/patel/ds/linkedlist/Demo.java
@@ -13,6 +13,15 @@ public class Demo {
             System.out.println("3.Search for an element");
             System.out.println("4.Insert at the beginging");
             System.out.println("5.Insert at the end");
+            System.out.println("6.Reference at the end");
+            System.out.println("7.Reference to predecessor Node");
+            System.out.println("8.Reference to ToAnElement");
+            System.out.println("9.Reference to PredecessorElement");
+            System.out.println("10.Reference to PartulularPosition");
+            System.out.println("11.Delete at the begining");
+            System.out.println("12.Delete at the end");
+            System.out.println("13.Delete after");
+            System.out.println("14.Reverse a linked list");
 
             System.out.println("Enter your choice(20 to exit): ");
             int choice = new Scanner(System.in).nextInt();
@@ -20,12 +29,51 @@ public class Demo {
             if(choice == 20) break;
             switch (choice) {
                 case 1: list.displayList(); break;
-                case 2: System.out.println("Size: " + list.count());; break;
+                case 2: System.out.println("Size: " + list.count()); break;
                 case 3: data = new Scanner(System.in).nextInt();list.search(data);
                     System.out.println(list.search(data));break;
                 case 4: data = new Scanner(System.in).nextInt();list.insertAtBegin(data);break;
                 case 5: data = new Scanner(System.in).nextInt();list.insertAtEnd(data);break;
+                case 6: if(list.referenceToLastNode() == null) {
+                	System.out.println("Not enough elements, Node size: " + list.count());
+                		} else {
+                			System.out.println("Got the last node having element: " + list.referenceToLastNode().data);
+                		}
+        		break;
+                case 7: if(list.referenceTo2ndLastNode() == null) {
+        			System.out.println("Not enough elements, Node size: " + list.count());
+        		} else {
+        			System.out.println("Got the 2nd last node having element: " + list.referenceTo2ndLastNode().data);
+        		}
+            	break;
+                case 8: data = new Scanner(System.in).nextInt();
+                		Node temp = list.referenceToAnElement(data);
+                		if(temp == null) {
+                			System.out.println("Not enough elements, Node size: " + list.count());
+                		} else {
+                			System.out.println("Got the  element: " + temp.data);
+                		}break;
+                case 9: data = new Scanner(System.in).nextInt();
+                		temp = list.referenceToPredecessorElement(data);
+		                if(temp == null) {
+		                	System.out.println("Not enough elements, Node size: " + list.count());
+                		} else {
+                			System.out.println("Got the predecessor element: " + temp.data);
+                		}
+		                break;
+                case 10:data = new Scanner(System.in).nextInt();
+                		temp = list.referenceAtPartulularPosition(data);
+                		if( temp == null) {
+                			System.out.println("Not enough elements, Node size: " + list.count());
+                		} else {
+                			System.out.println("Got the particular position element: " + temp.data);
+                		}break;
+                case 11: list.deleteAtBegin();break;
+                case 12: list.deleteAtEnd();break;
+                case 13: data = new Scanner(System.in).nextInt();list.deleteAfter(data);break;
+                case 14: list.reverseRecursive();break;
                 default: break;
+                
             }
         }
 
--- src/main/java/com/patel/ds/linkedlist/SingleLinkedList.java
+++ src/main/java/com/patel/ds/linkedlist/SingleLinkedList.java
@@ -23,11 +23,125 @@ public class SingleLinkedList {
         }
         System.out.println();
     }
+    
+    public Node referenceToLastNode() {
+    	if(start == null) {
+            System.out.println("List is empty");
+            return null;
+        }
+        Node p;
+        p = start;
+        while (p.link != null) {
+            p = p.link;
+        }
+        return p;
+    }
+    
+    public Node referenceToAnElement(int data) {
+    	if(start == null) {
+            System.out.println("List is empty");
+            return null;
+        }
+        Node p;
+        p = start;
+        while (p.data != data) {
+            p = p.link;
+        }
+        return p;
+    }
+    
+    public Node referenceToPredecessorElement(int data) {
+    	if(start == null) {
+            System.out.println("List is empty");
+            return null;
+        }
+        Node p;
+        p = start;
+        while (p.link.data != data) {
+            p = p.link;
+        }
+        return p;
+    }
+    
+    public Node referenceAtPartulularPosition(int position) {
+    	if(start == null) {
+            System.out.println("List is empty");
+            return null;
+        }
+        Node p;
+        p = start;
+        for(int i = 1; i < position && p.link != null; i++) {
+        	p = p.link;
+        }
+        return p;
+    }
+    
+    public Node referenceTo2ndLastNode() {
+    	Node p;
+        p = start;
+    	if(p == null || p.link == null) {
+            System.out.println("List is empty or there is only one element ");
+            return null;
+        }
+        while (p.link.link != null) {
+            p = p.link;
+        }
+        return p;
+    }
 
     public void insertAtBegin(int data) {
         Node temp = new Node(data);
         temp.link = start;
         start = temp;
+    }
+    
+    public void deleteAtBegin() {
+    	if(start != null) {
+    		start = start.link;
+    	}
+    }
+    
+    public void deleteAtEnd() {
+    	Node p = start;
+    	while(p.link.link != null) {
+    		p = p.link;
+    	}
+    	p.link = null;
+    }
+    
+    public void deleteAfter(int data) {
+    	Node p = referenceToPredecessorElement(data);
+    	p.link = p.link.link;
+    	
+    }
+    
+    public Node reverse() {
+    	Node prev = null,next,current;
+    	current = start;
+    	
+    	while(current != null) {
+    		next = current.link;
+        	current.link = prev;
+        	prev = current;
+        	current = next;
+    	}
+    	start = prev;
+    	return start;
+    }
+    public void reverseRecursive() {reverseRecursive(start);}
+    public Node reverseRecursive(Node start) {
+    	if(start == null) return start;
+    	
+    	else {
+    		Node prev=null,next,current;
+    		current = reverseRecursive(start.link);
+    		next = current.link;
+        	current.link = prev;
+        	prev = current;
+        	current = next;
+    		
+    		return prev;
+    	}
     }
 
     public void insertAtEnd(int data) {
@@ -53,7 +167,7 @@ public class SingleLinkedList {
         }
         return count;
     }
-
+ 
     public boolean search(int data) {
         Node p = start;
         while (p != null) {
